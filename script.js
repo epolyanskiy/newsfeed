17c61b77e5845340a0761cd37156c8f779794780
егорка <142446788+epolyanskiy@users.noreply.github.com>
2025-05-09 00:25:45 +0300

    Module 2 block 2 part 2.2

--- script.js
+++ script.js
@@ -196,33 +196,80 @@ const data = {
 const mainNews = data.items.slice(0, 3);
 const smallNews = data.items.slice(3, 12);
 
-const mainNewsTemplate = document.getElementById('main-news-item');
-const smallNewsTemplate = document.getElementById('small-article-item');
 const mainNewsContainer = document.querySelector('.articles__big-column');
 const smallNewsContainer = document.querySelector('.articles__small-column');
 
+const createMainNewsItem = (item) => {
+    const categoryData = data.categories.find((categoryItem => categoryItem.id == item.category_id));
+    const sourceData = data.sources.find((sourceItem) => sourceItem.id == item.source_id);
+
+    const article = document.createElement('article');
+    const imageContainer = document.createElement('div');
+    const image = document.createElement('img');
+    const content = document.createElement('div');
+    const category = document.createElement('span');
+    const title = document.createElement('h2');
+    const text = document.createElement('p');
+    const source = document.createElement('span');
+
+    article.classList.add('main-article');
+    imageContainer.classList.add('main-article__image-container');
+    image.classList.add('main-article__image');
+    content.classList.add('main-article__content');
+    category.classList.add('article-category', 'main-article__category');
+    title.classList.add('main-article__title');
+    text.classList.add('main-article__text');
+    source.classList.add('article-source', 'main-article__source');
+
+    title.textContent = item.title;
+    category.textContent = categoryData.name;
+    image.src = item.image;
+    text.textContent = item.description;
+    source.textContent = sourceData.name;
+
+    imageContainer.appendChild(image);
+    article.appendChild(imageContainer);
+    content.appendChild(category);
+    content.appendChild(title);
+    content.appendChild(text);
+    content.appendChild(source);
+    article.appendChild(content);
+
+    return article;
+}
+
+const createSmallNewsItem = (item) => {
+    const sourceData = data.sources.find((sourceItem) => sourceItem.id == sourceItem.id);
+    const dateData = new Date(item.date).toLocaleDateString('ru-RU', {month: 'long', day: 'numeric'});
+
+    const article = document.createElement('article');
+    const title = document.createElement('h2');
+    const textContainer = document.createElement('p');
+    const date = document.createElement('span');
+    const source = document.createElement('span');
+
+    article.classList.add('small-article');
+    title.classList.add('small-article__title');
+    textContainer.classList.add('small-article__caption');
+    date.classList.add('article-date', 'small-article__date');
+    source.classList.add('article-source', 'small-article__source');
+
+    title.textContent = item.title;
+    source.textContent = sourceData.name;
+    date.textContent = dateData;
+
+    textContainer.appendChild(date);
+    textContainer.appendChild(source);
+    article.appendChild(title);
+    article.appendChild(textContainer);
+
+    return article;
+}
+
 mainNews.forEach(item => {
-    const element = mainNewsTemplate.content.cloneNode(true);
-    const category = data.categories.find((categoryItem => categoryItem.id == item.category_id));
-    const source = data.sources.find((sourceItem) => sourceItem.id == sourceItem.id);
-
-    element.querySelector('.main-article__title').textContent = item.title;
-    element.querySelector('.main-article__image').src = item.image;
-    element.querySelector('.main-article__category').textContent = category.name;
-    element.querySelector('.main-article__text').textContent = item.description;
-    element.querySelector('.main-article__source').textContent = source.name;
-
-    mainNewsContainer.appendChild(element);
+    mainNewsContainer.appendChild(createMainNewsItem(item));
 })
 
 smallNews.forEach(item => {
-    const element = smallNewsTemplate.content.cloneNode(true);
-    const source = data.sources.find((sourceItem) => sourceItem.id == sourceItem.id);
-    const date = new Date(item.date).toLocaleDateString('ru-Ru', {month: 'long', day: 'numeric'});
-
-    element.querySelector('.small-article__title').textContent = item.title;
-    element.querySelector('.small-article__date').textContent = date;
-    element.querySelector('.small-article__source').textContent = source.name;
-
-    smallNewsContainer.appendChild(element);
+    smallNewsContainer.appendChild(createSmallNewsItem(item));
 })
